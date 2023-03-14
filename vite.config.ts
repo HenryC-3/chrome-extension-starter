9be1de5d3aa201e3f76af7c7d9668c15f545e87c
HenryC-3 <2324375439@qq.com>
2023-03-14 17:50:13 +0800

    chore: update package name

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,13 @@ import { crx } from "@crxjs/vite-plugin";
 import manifest from "./src/manifest.config";
 import zipPack from "vite-plugin-zip-pack";
 import packageJson from "./package.json";
-const { version } = packageJson;
+const { version, name } = packageJson;
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [
 		vue(),
 		crx({ manifest }),
-		zipPack({ outDir: "./dist-zip", outFileName: `d2n-v${version}.zip` }),
+		zipPack({ outDir: "./dist-zip", outFileName: `${name}-v${version}.zip` }),
 	],
 });
